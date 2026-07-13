cf1c5e4afec1289d210cb970e31429c2152e55fd
dulstack <doowew100@gmail.com>
2026-07-13 06:34:20 +0200

    Use only 4 pins for uart

--- src/uart.c
+++ src/uart.c
@@ -2,30 +2,24 @@
 #include"gpio.h"
 bool uart_init(uint32_t baudrate){
 	uint32_t CR;
-	#ifndef __QEMU_BOARD
-	for(int pin = 14; pin <= 39; pin++){
-		//don't forget to jump over the pins 18-29, 34, 35
-		if((pin > 17 && pin < 30) ||
-		   (pin > 33 && pin < 36) 
-		) continue;
+	//Set uart0 alternate funcitons for pins 30-33 to ALT3
+	for(int pin = 30; pin < 34; pin++){
 		//here the gpio_pull_* calls will slow down the CPU a bit. TODO: optimize this part of code
-		pin==36 ? gpio_pull_up(pin) : gpio_pull_down(pin);
-		if(pin < 16)gpio_set_function(pin, GPIO_FUNC_ALT0);
-		else gpio_set_function(pin, pin < 36 ? GPIO_FUNC_ALT3 : GPIO_FUNC_ALT2);
+		gpio_pull_down(pin);
+		gpio_set_function(pin, GPIO_FUNC_ALT3);
 
 	}
-	#endif
 	*(UART_BASE + UART_CR) &= ~(0xffff);
 	//wait until the end of transmission
 	while( *(UART_BASE + UART_FR) & (1 << 3));
-	*(UART_BASE + UART_LCRH) &= ~(1<<4); //flush the TX FIFO
+	*(UART_BASE + UART_LCRH) &= ~(1 << 4); //flush the TX FIFO
 	CR = *(UART_BASE);
 	CR |= 0x300;
 	*(UART_BASE) = CR;
 	//set word length to 8 bits and enable FIFOs 
 	*(UART_BASE + UART_LCRH) |= 0x70; 
 	*(UART_BASE + UART_IBRD) = (FUARTCLK / (16 * baudrate) ) & 0xffff;
-	int frac = (FUARTCLK % (16 * baudrate)); 
+	int frac = (FUARTCLK % (16 * baudrate));
 	*(UART_BASE + UART_FBRD) = frac & 0x3d;
 }
 void uart_putc(char c){
@@ -37,4 +31,9 @@ char uart_getc(){
 	//wait for RX FIFO to free
 	while( *(UART_BASE + UART_FR) & 1<<5 );
 	return *(UART_BASE + UART_DR);
+}
+size_t uart_write(const char* buf, size_t count){
+	while(count-- > 0){
+		uart_putc(*(buf++));
+	}
 }
