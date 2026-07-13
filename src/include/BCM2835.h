ba5745fbb0cfdedcc7799fd1270f5346821b8df0
dulstack <doowew100@gmail.com>
2026-07-13 06:35:43 +0200

    Fix the code format and include stddef.h

--- src/include/BCM2835.h
+++ src/include/BCM2835.h
@@ -2,7 +2,8 @@
 #define __BCM2835_H
 #define MMIO_BASE 0x20000000
 #define __NOP(count)\
-	for(int i=count;i>0;i--)__asm__("nop");
+	for(int i = count; i > 0; i--)__asm__("nop");
 #include <stdint.h>
 #include <stdbool.h>
+#include <stddef.h>
 #endif
